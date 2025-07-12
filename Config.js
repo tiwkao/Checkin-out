92ab9290073ee891dd938616373b5414e16116e1
tiwkao <pongsakorn.592040105@gmail.com>
2025-07-12 20:54:22 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,7 +1,7 @@
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbxKtQXKqAOxIOb8Pgh6Gg3h6-YHj8WnPTC0Lhl-Gq_aFSfWXoxfMRSo2vKFY7pa8yP6EQ/exec';
-    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwLkcQkCQppkiiGRrGT6yN6O4KPbPaJAeOlgtv3u6yGh2aF0ubVNgIi4oDIyslajxUj/exec';
+    const WEB_APP_MEMBER_URL = https://script.google.com/macros/s/AKfycbwnHYI8MCVdVZOfwuUPWCi-bFmQOh8J7wbjck2hgCJzSSTTUhxPZp6IxBdbvjfM8GDNAQ/exec';
+    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzFHZHSkSEsTElSf4-ZvUbDAIohlosLt9M7xnk2iYgaOZocuit_pkG4tMUP7srEOovW/exec';
 //LIFF_ID
-    const LIFF_ID = '2006740306-qp2xNvgM'; // LIFF ID Checkin
-    const LIFF_HISTORY = '2006740306-qLWyP3w6'; // LIFF ID history
-    const LIFF_REGISTER = '2006740306-qlr7J9go'; // LIFF ID register
+    const LIFF_ID = 'XX'; // LIFF ID Checkin
+    const LIFF_HISTORY = 'XXX'; // LIFF ID history
+    const LIFF_REGISTER = 'XX'; // LIFF ID register
