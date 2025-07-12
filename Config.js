7181aadb3075553f9a45479898a1e75366ba74ce
tiwkao <pongsakorn.592040105@gmail.com>
2025-07-12 20:56:41 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,6 +2,6 @@
     const WEB_APP_MEMBER_URL = https://script.google.com/macros/s/AKfycbwnHYI8MCVdVZOfwuUPWCi-bFmQOh8J7wbjck2hgCJzSSTTUhxPZp6IxBdbvjfM8GDNAQ/exec';
     const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzFHZHSkSEsTElSf4-ZvUbDAIohlosLt9M7xnk2iYgaOZocuit_pkG4tMUP7srEOovW/exec';
 //LIFF_ID
-    const LIFF_ID = 'XX'; // LIFF ID Checkin
+    const LIFF_ID = '2007656229-B9EAzV8v'; // LIFF ID Checkin
     const LIFF_HISTORY = 'XXX'; // LIFF ID history
     const LIFF_REGISTER = 'XX'; // LIFF ID register
