3959f612f0a42cdf7026a9f2d77e91fa136449ad
tiwkao <pongsakorn.592040105@gmail.com>
2026-07-19 16:52:08 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,6 +1,6 @@
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
     const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbw_ktYP0np8zcBug6yDe2CxEWPnCKabIzXS6WLw8ebTuue_hONqY2zSbLkarL55CQMklw/exec';
-    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbye_HZ-yHR9OQLA-a1HI0xiM7pEzEkZXuK1Ca8DLfuVmyIk-6ms_AEHurC965R4QSVf/exec';
+    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyAo2yguMrI6Vj27oXQhkxCX4tc2CBzWjNE89fHp_LLqmt2a8FYUY8tjxiI_WfORRFG/exec';
 //LIFF_ID
     const LIFF_ID = '2007656229-B9EAzV8v'; // LIFF ID Checkin
     const LIFF_HISTORY = '2007656229-8nnlMGYV'; // LIFF ID history
